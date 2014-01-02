c9e35419ad40574a780a53c7f74bbcf1262eaaf3
too_bad <too_bad@too_bad-PC>
2014-01-02 20:32:10 +0800

    diff test

--- slark-test/src/test/DiffDiary.scala
+++ slark-test/src/test/DiffDiary.scala
@@ -4,8 +4,9 @@ import slark.Diff._
 
 object DiffDiary extends Diary {
 
-  val content = Source(diff("", "")) :: Source(diff("", "1")) :: Source(diff("1", "")) :: Source(diff("1", "1")) :: 
-  Source(diff("abcabba", "cbabac")) :: Nil
+  val content = 
+    Source(diff("", "")) :: Source(diff("", "1")) :: Source(diff("1", "")) :: Source(diff("1", "1")) :: 
+    Source(diff("abcabba", "cbabac")) :: Source(diff("cbabac", "abcabba")) :: Nil
   
   def diff(source: String, modified: String): Scripts = MyersDiff(source, modified)(_ equals _)
   
