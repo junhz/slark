8ca969359a408342e89a1ddb2242b8affb6e52b4
a554114 <a554114@HZPCIT-R8DN15G.corp.statestr.com>
2014-01-02 16:53:03 +0800

    add test for diff

---
+++ slark-test/src/test/DiffDiary.scala
@@ -0,0 +1,12 @@
+package test
+
+import slark.Diff._
+
+object DiffDiary extends Diary {
+
+  val content = Source(diff("", "")) :: Source(diff("", "1")) :: Source(diff("1", "")) :: Source(diff("1", "1")) :: 
+  Source(diff("abcabba", "cbabac")) :: Nil
+  
+  def diff(source: String, modified: String): Scripts = MyersDiff(source, modified)(_ equals _)
+  
+}
