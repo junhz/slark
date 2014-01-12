71b456f5e177746f2cb8c20ab29b13c6339f924a
too_bad <too_bad@too_bad-PC>
2014-01-12 20:25:55 +0800

    funcLib

--- slark-test/src/test/FuncLibDiary.scala
+++ slark-test/src/test/FuncLibDiary.scala
@@ -5,9 +5,17 @@ import slark.FuncLib._
 object FuncLibDiary extends Diary {
 
   val content =
-    Source((open2 `with` 0)(sum)(1, 2)) ::
-    Source((open2 and as[Int])(sum)((1, 2), 3)) :: Nil
-
+    Source((open2 `with` 3 `then` sum)(1, 2)) ::
+    Source((open2 and as[Int] `then` sum)((1, 2), 3)) :: 
+    Source((open2 `then` cons[Int])(1, Nil)) :: 
+    Source(optEx(None, 3)) :: 
+    Source(optEx(Some(1, None), 3)) :: 
+    Source(optEx(Some(1, Some(2)), 3)) :: Nil
+    
   val sum: (Int, Int, Int) => String = (a: Int, b: Int, c: Int) => s"$a + $b + $c"
+  
+  def cons[T]: (T, List[T]) => List[T] = (head: T, tail: List[T]) => head :: tail
 
+  val optEx = ((as[Int] and as[Int].default(2)).default(1, 2) and as[Int] `then` sum)
+  
 }
--- slark/src/slark/FuncLib.scala
+++ slark/src/slark/FuncLib.scala
@@ -2,32 +2,47 @@ package slark
 
 object FuncLib {
   
-  abstract class Funct1[A, F] {
-    def apply[U](f: A => U): F => U
+  abstract class Funct1[A, F] { self =>
+    def `then`[U](f: A => U): F => U
+    final def and[B, F_](that: Funct1[B, F_]): Funct2[A, B, (F, F_)] = new Funct2[A, B, (F, F_)] {
+      def `then`[U](f: A => B => U): ((F, F_)) => U = (ff: (F, F_)) => that.`then`(self.`then`(f)(ff._1))(ff._2)
+    }
+    final def default(a: A): Funct1[A, Option[F]] = new Funct1[A, Option[F]] {
+      override def `then`[U](f: A => U): Option[F] => U = _ match {
+        case None => f(a)
+        case Some(s) => self.`then`(f)(s)
+      }
+    }
   }
   
   abstract class Funct2[A, B, F] { self =>
-    def apply[U](f: A => B => U): F => U
-    final def apply[U](f: (A, B) => U): F => U = apply(f.curried)
+    def `then`[U](f: A => B => U): F => U
+    final def `then`[U](f: (A, B) => U): F => U = `then`(f.curried)
     final def `with`[T](t: T): Funct3[A, B, T, F] = new Funct3[A, B, T, F] {
-      def apply[U](f: A => B => T => U): F => U = (ff: F) => self.apply(f)(ff)(t)
+      def `then`[U](f: A => B => T => U): F => U = (ff: F) => self.`then`(f)(ff)(t)
     }
     final def and[C, F_](that: Funct1[C, F_]): Funct3[A, B, C, (F, F_)] = new Funct3[A, B, C, (F, F_)] {
-      def apply[U](f: A => B => C => U): ((F, F_)) => U = (ff: (F, F_)) => that.apply(self.apply(f)(ff._1))(ff._2)
+      def `then`[U](f: A => B => C => U): ((F, F_)) => U = (ff: (F, F_)) => that.`then`(self.`then`(f)(ff._1))(ff._2)
+    }
+    final def default(a: A, b: B): Funct2[A,B, Option[F]] = new Funct2[A, B, Option[F]] {
+      override def `then`[U](f: A => B => U): Option[F] => U = _ match {
+        case None => f(a)(b)
+        case Some(s) => self.`then`(f)(s)
+      }
     }
   }
   
   abstract class Funct3[A, B, C, F] {
-    def apply[U](f: A => B => C => U): F => U
-    final def apply[U](f: (A, B, C) => U): F => U = apply(f.curried)
+    def `then`[U](f: A => B => C => U): F => U
+    final def `then`[U](f: (A, B, C) => U): F => U = `then`(f.curried)
   }
   
   def as[T]: Funct1[T, T] = new Funct1[T, T] {
-    def apply[U](f: T => U): T => U = (t: T) => f(t)
+    def `then`[U](f: T => U): T => U = (t: T) => f(t)
   }
   
   def open2[A, B]: Funct2[A, B, (A, B)] = new Funct2[A, B, (A, B)] {
-    override def apply[U](f: A => B => U): ((A, B)) => U = (tuple: (A, B)) => f(tuple._1)(tuple._2)
+    override def `then`[U](f: A => B => U): ((A, B)) => U = (tuple: (A, B)) => f(tuple._1)(tuple._2)
   }
   
 }
