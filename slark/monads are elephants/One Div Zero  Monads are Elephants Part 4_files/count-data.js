bcfbf44c0e0ad7415a093c733d23b32c39c630f1
too_bad <too_bad@too_bad-PC>
2014-03-31 20:21:39 +0800

    resouce

---
+++ slark/monads are elephants/One Div Zero  Monads are Elephants Part 4_files/count-data.js
@@ -0,0 +1,5 @@
+var DISQUSWIDGETS;
+
+if (typeof DISQUSWIDGETS != 'undefined') {
+    DISQUSWIDGETS.displayCount({"text":{"and":"and","comments":{"zero":"0 Comments","multiple":"{num} Comments","one":"1 Comment"}},"counts":[{"id":"http:\/\/james-iry.blogspot.com\/2007\/11\/monads-are-elephants-part-4.html","comments":36}]});
+}
